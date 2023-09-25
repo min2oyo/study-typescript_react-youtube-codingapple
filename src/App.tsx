75c85dd6550ff9a13de0496610740b5d4f95d1bc
min2oyo <min2u@outlook.com>
2023-09-25 16:25:36 +0900

    완강

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import Profile from "./components/Profile";
+import { increment, RootState } from './index';
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  const 꺼내온거 = useSelector((state: RootState) => state);
 
   return (
     <div className="App">
       <h4>안녕하십니까</h4>
       <Profile name={`ina`} age={20} />
+      {꺼내온거.counter1.count}
+      <button onClick={() => { dispatch(increment()); }}>버튼</button>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,48 @@
+import { Provider } from 'react-redux';
+
 import ReactDOM from 'react-dom/client';
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import App from './App';
 
 import './index.css';
+
+const 초기값 = { count: 0, user: 'kim' };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 초기값,
+  reducers: {
+    increment(state) {
+      state.count += 1;
+    },
+    decrement(state) {
+      state.count -= 1;
+    },
+    incrementByAmount(state, action: PayloadAction<number>) {
+      state.count += action.payload;
+    }
+  }
+});
+
+const store = configureStore({
+  reducer: {
+    counter1: counterSlice.reducer
+  }
+});
+
+//state 타입을 export 해두는건데 나중에 쓸 데가 있음
+export type RootState = ReturnType<typeof store.getState>;
+
+//수정방법 만든거 export
+export let { increment, decrement, incrementByAmount } = counterSlice.actions;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
